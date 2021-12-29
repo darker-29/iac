1903ca39d123557ab26146abef788076058d76fd
darker-29 <darker29.developer@gmail.com>
2021-12-29 23:49:36 +0900

    add: aws,terraform同一のリソース作成テンプレートを追加

---
+++ terraform/template.tf
@@ -0,0 +1,116 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Name-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Name-igw"
+  }
+}
+
+resource "aws_subnet" "pub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Name-pubsub"
+  }
+}
+
+resource "aws_route_table" "pub" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Name-pub-routetable"
+  }
+}
+
+resource "aws_route" "pub" {
+  route_table_id         = aws_route_table.pub.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+  depends_on             = [aws_route_table.pub]
+}
+
+resource "aws_route_table_association" "pub" {
+  subnet_id      = aws_subnet.pub.id
+  route_table_id = aws_route_table.pub.id
+}
+
+resource "aws_subnet" "pri" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Name-prisub"
+  }
+}
+
+resource "aws_route_table" "pri" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Name-pri-routetable"
+  }
+}
+
+resource "aws_route_table_association" "pri" {
+  subnet_id      = aws_subnet.pri.id
+  route_table_id = aws_route_table.pri.id
+}
+
+resource "aws_security_group" "web" {
+  name        = "Name-web-secgroup"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "db" {
+  name        = "Name-db-secgroup"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
