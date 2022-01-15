20043ae93e8545db53d1c3a496a85cfca26f916e
darker-29 <darker29.developer@gmail.com>
2022-01-15 18:49:19 +0900

    add: required_provider

--- terraform/template.tf
+++ terraform/template.tf
@@ -1,7 +1,12 @@
 terraform {
   required_version = ">=1.1.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
 }
-
 
 provider "aws" {
   region = "ap-northeast-1"
